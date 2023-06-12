5ead04f61c45fe67622e57f305c9dbd995ee41e9
Vinoth Kanna <98391866+vinothkanna273@users.noreply.github.com>
2023-06-12 23:48:27 +0530

    Update PPEDetection.py

--- PPEDetection.py
+++ PPEDetection.py
@@ -3,10 +3,14 @@ import cv2
 import cvzone
 import math
 
-cap = cv2.VideoCapture(0)  # For Webcam
-cap.set(3, 1280)
-cap.set(4, 720)
-# cap = cv2.VideoCapture("../Videos/ppe-1.mp4")  # For Video
+
+# For Video
+cap = cv2.VideoCapture("ppe-1.mp4")  
+
+# For Webcam
+# cap = cv2.VideoCapture(0) 
+# cap.set(3, 1280)
+# cap.set(4, 720)
 
 model = YOLO("ppe.pt")
 
@@ -22,9 +26,7 @@ while True:
             # Bounding Box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
             w, h = x2 - x1, y2 - y1
-            # cvzone.cornerRect(img, (x1, y1, w, h))
             # Confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
             # Class Name
